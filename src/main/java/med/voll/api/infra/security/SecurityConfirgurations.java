e655f26129575fafc3e62dd3709a74e203de82d8
Seu Nome <marcoadias.silva@gmail.com>
2025-05-29 09:44:29 -0300

    feat: novos filtros, geracao  de token, validacao de token, aplicacao stateless

--- src/main/java/med/voll/api/infra/security/SecurityConfirgurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfirgurations.java
@@ -3,23 +3,41 @@ package med.voll.api.infra.security;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfirgurations {
 
+    private final SecurityFilter apiKeyFilter;
+
+    public SecurityConfirgurations(SecurityFilter apiKeyFilter) {
+        this.apiKeyFilter = apiKeyFilter;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authorize) ->
+                        authorize
+                                .requestMatchers("/login").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(apiKeyFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 
